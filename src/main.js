68854595e50926d13cd7ca60d5a87169e56906d1
carapai <olupotcharles@gmail.com>
2019-02-12 17:36:57 +0300

    Corrected error in column and row not found

--- src/main.js
+++ src/main.js
@@ -599,7 +599,7 @@ const processData = (data, foundEntities) => {
                     const optionsSet = a.trackedEntityAttribute.optionSet;
                     const validatedValue = validateValue(type, value, optionsSet);
                     const row = client.client;
-                    const column = e.column.value;
+                    const column = a.column.value;
                     if (value !== '' && validatedValue !== null) {
                         attributes = [...attributes, {
                             attribute: a.trackedEntityAttribute.id,
